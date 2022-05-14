a25076acc042b16d657afe3898e586e41b29290f
akhmetovai <akhmet.btc@yandex.com>
2022-05-15 02:12:18 +0300

    add synt data for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import os
+from typing import List
+from collections import defaultdict
 
 import pytest
-from typing import List
+import numpy as np
+import pandas as pd
 
 
 @pytest.fixture()
@@ -29,9 +32,47 @@ def categorical_features() -> List[str]:
     ]
 
 
-@pytest.fixture
+@pytest.fixture()
 def numerical_features() -> List[str]:
     return [
         "age", "trestbps",
         "chol", "thalach", "oldpeak"
     ]
+
+
+@pytest.fixture()
+def generated_data() -> List[str]:
+    return [
+        "age", "trestbps",
+        "chol", "thalach", "oldpeak"
+    ]
+
+
+@pytest.fixture()
+def synthetic_data():
+    np.random.seed(42)
+
+    synt_data = defaultdict(list)
+
+    for _ in range(1000):
+        row = {
+            "age": int(9 * np.random.randn() + 54),
+            "sex": np.random.choice([0, 1], p=[0.33, 0.67]),
+            "cp": np.random.choice([0, 1, 2, 3], p=[0.08, 0.16, 0.28, 0.48]),
+            "trestbps": int(17 * np.random.randn() + 131),
+            "chol": int(51 * np.random.randn() + 247),
+            "fbs": np.random.choice([0, 1], p=[0.85, 0.15]),
+            "restecg": np.random.choice([0, 1, 2], p=[0.48, 0.04, 0.48]),
+            "thalach": int(22 * np.random.randn() + 149),
+            "exang": np.random.choice([0, 1], p=[0.67, 0.33]),
+            "oldpeak": max(1.16 * np.random.randn() + 1.05, 0),
+            "slope": np.random.choice([0, 1, 2], p=[0.46, 0.46, 0.08]),
+            "ca": np.random.choice([0, 1, 2, 3], p=[0.58, 0.22, 0.13, 0.07]),
+            "thal": np.random.choice([0, 1, 2], p=[0.55, 0.39, 0.06]),
+            "condition": np.random.choice([0, 1], p=[0.53, 0.47]),
+        }
+        for column, value in row.items():
+            synt_data[column].append(value)
+
+    synt_data = pd.DataFrame(synt_data)
+    return synt_data
--- tests/model_utils/test_model_utils.py
+++ tests/model_utils/test_model_utils.py
@@ -1,5 +1,6 @@
 from hydra import initialize, compose
 from sklearn.utils.validation import check_is_fitted
+import pandas as pd
 
 from ml_project.data.utils import read_data, split_train_val_data
 from ml_project.models.utils import train_model
@@ -20,6 +21,26 @@ def test_train_model(
         )
         train, test = split_train_val_data(
             data, random_state=42, test_size=0.2, shuffle=True
+        )
+        train_target = extract_target(train, cfg.features.target_col)
+        test_target = extract_target(test, cfg.features.target_col)
+        train = train.drop(cfg.features.target_col, axis=1)
+        test = test.drop(cfg.features.target_col, axis=1)
+
+        model = train_model(train, train_target, cfg)
+        check_is_fitted(model)
+
+        prediction = model.predict(test)
+        assert prediction.shape[0] == test_target.shape[0]
+
+
+def test_train_model_on_generated_data(
+    synthetic_data: pd.DataFrame,
+):
+    with initialize(config_path="../../ml_project/conf"):
+        cfg = compose(config_name="config",)
+        train, test = split_train_val_data(
+            synthetic_data, random_state=42, test_size=0.2, shuffle=True
         )
         train_target = extract_target(train, cfg.features.target_col)
         test_target = extract_target(test, cfg.features.target_col)
